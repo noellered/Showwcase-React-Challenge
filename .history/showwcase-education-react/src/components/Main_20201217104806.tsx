548f5b714dbdb311e979f0f200b9bd8f58e60fcf
Noelle Szombathy <business.noellered@gmail.com>
2020-12-17 11:21:42 -0800

    Modal close after form submit

---
+++ .history/showwcase-education-react/src/components/Main_20201217104806.tsx
@@ -0,0 +1,57 @@
+import React, {FunctionComponent, useState} from 'react';
+import EducationSidebar from './education/EducationSidebar';
+import EducationList from './education/EducationList';
+import EducationModalContent from './education/EducationModalContent';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Paper from '@material-ui/core/Paper';
+
+const Main:FunctionComponent<{name: string}> = ({name}) => {
+    const [open, setOpen] = useState<boolean>(false)
+    const [education, setEducation] = useState<object[]>([])
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleEducationUpdate = (newEducation) => {
+        if(newEducation.institution !== null) {
+            setEducation(education.concat(newEducation))
+
+        }
+    }
+
+    console.log('Education', education);
+
+    return(
+        <div>
+            <h4>Welcome to {name}'s education page.</h4>
+            <button type="button" onClick={handleOpen}>Add New Education</button>            
+            <EducationSidebar/> 
+            <EducationList/>
+            <Modal aria-labelledby="add-education"
+                aria-describedby="add-education-modal"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 300,
+                }}>
+                <Fade in={open}>
+                    <Paper>
+                        <EducationModalContent handleUpdate={handleEducationUpdate}/>    
+                    </Paper>
+                </Fade>
+            </Modal>
+           
+        </div>
+    )
+}
+
+export default Main;
